06777533e9cd48285aa17e3f193c8423e2ae5c40
Shreya Shah <59884864+diverheart@users.noreply.github.com>
2021-07-01 04:54:43 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -80,7 +80,6 @@ app.get('/error',function(req,res)
 {
      res.sendFile(path.join(__dirname+'/error.html'));
 });
-app.listen(3000, function()
-	{
-		console.log("Server connected to port 3000");
-	});
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
