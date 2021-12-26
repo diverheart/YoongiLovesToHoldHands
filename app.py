239b3d214be016dbbee00427fa4efc848a931503
Shreya Shah <59884864+diverheart@users.noreply.github.com>
2021-12-26 21:07:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import tweepy
 import sys
 
-auth = tweepy.OAuthHandler("gwqXIH4fqmYMv9aZLwk2cXVlI", "W5SE4xoUSG0qw2HUa4XfL7vKjXyAHFrI2P7CqLdcVLm68GWXl9")
-auth.set_access_token("1404195853791727617-xo6abiRTbmIboJl4Xvq36XHpK7WvdH", "Mt3RE9sFEUoysCPtGc3gN0zenSmLxW3ZS0soKk6YYrRX0")
+auth = tweepy.OAuthHandler("k9cdIm9ZpavDuNMYY0V9dYPCx", "2btkzqiZAQ3SdreBDYNQJjmWzGWsOk6FmkvOPWLKUqvZn49pbp")
+auth.set_access_token("1404195853791727617-UDHm5AJ8GgB5KKl376FK7al6fP0EVW", "ynhYEpJORDrfaynNV6PMFg5xMnrUgfnSrwnSp2uxMmY4v")
 api = tweepy.API(auth)
 
 
